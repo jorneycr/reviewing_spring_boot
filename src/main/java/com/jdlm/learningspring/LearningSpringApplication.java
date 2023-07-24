7a56e6d785587c4321edbbf2af29e82407627676
jorneylopez <jorney95@live.com>
2023-07-24 12:27:33 -0600

    Project completed

---
+++ src/main/java/com/jdlm/learningspring/LearningSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jdlm.learningspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringApplication {
+
+	public static void main(String[] args) {
+		System.out.println(SpringApplication.run(LearningSpringApplication.class, args));
+	}
+
+}
